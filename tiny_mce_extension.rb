9d53327e781ef95798a4d618bcc109d4891ce0ab
Pat Allan <pat@freelancing-gods.com>
2009-04-05 20:23:33 +1000

    Making sure the extension doesn't get in the way of bootstrapping

--- tiny_mce_extension.rb
+++ tiny_mce_extension.rb
@@ -25,7 +25,9 @@ class TinyMceExtension < Radiant::Extension
     # We don't want any Content Type limitations. Default for Paperclipped
     # does not include PDFs, Word and Excel Documents - things that quite
     # likely will be linked to.
-    Radiant::Config['assets.content_types'] = ""
+    if ActiveRecord::Base.connection.tables.include?("config")
+      Radiant::Config['assets.content_types'] = ""
+    end
     
     ApplicationController.class_eval  { include TinyMce::ControllerMixin }
     Admin::PagesController.class_eval { include TinyMce::PagesControllerMixin }
